486cd2027848ab7033b155ff6931b66c4f566f63
Blake <rblakehampton@gmail.com>
2022-06-28 16:28:17 -0500

    center

---
+++ src/components/CenterContent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+export const CenterContent = ({children}) => {
+  return (
+    <>
+      <div>
+        <p>THIS IS A TEST</p>
+        {children}
+      </div>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,29 +5,13 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import MatterHeader from "../components/MatterHeader"
+import { CenterContent } from "../components/CenterContent"
+import { Children } from "react"
 
 const IndexPage = () => (
-<Layout>
-    <Seo title="Home" />
-    <MatterHeader/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+<CenterContent>
+  <h1>HELLO World</h1>
+</CenterContent>
 )
 
 export default IndexPage
