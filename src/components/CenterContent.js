6d54b126c04f6ace8650854ce6f64d4b65901f54
rblakeh <rblakehampton@gmail.com>
2022-06-28 21:56:53 -0500

    media

--- src/components/CenterContent.js
+++ src/components/CenterContent.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react"
+import { css, cx } from '@emotion/css'
+
+const balls = css`
+background: red;
+margin: auto;
+@media (min-width: 1201px){
+  width: 60%;
+}
+@media (min-width: 1025px){
+  width: 70%;
+}
+@media (min-width: 769px){
+  width: 80%;
+}
+@media (min-width: 481px){
+  width: 90%;
+}
+@media (min-width: 320px){
+  width: 100%;
+}
+`
 
 export const CenterContent = ({children}) => {
   return (
     <>
-      <div>
+      <div className={balls}>
         <p>THIS IS A TEST</p>
         {children}
       </div>
