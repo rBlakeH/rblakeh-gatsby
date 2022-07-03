0416f5af5d14d95f24ccc56f7ff88e032cdcbace
Chris Salinas Jr <chrissalinasjr@gmail.com>
2022-07-03 15:14:15 -0600

    fix(matter): responsive

--- src/components/CenterContent.tsx
+++ src/components/CenterContent.tsx
@@ -1,33 +1,32 @@
-import React, { Component } from "react"
-import { css, cx } from '@emotion/css'
+import React, { PropsWithChildren } from "react"
+import { css } from "@emotion/css"
 
-type Props ={
-  children: any
-  ref: any
-}
+type Props = {}
 
-const center = css`
-border: 5px solid black;
-margin: auto;
-width: 100%;
-@media (min-width: 480px){
-  width: 416px;
-}
-@media (min-width: 768px){
-  width: 704px;
-}
-@media (min-width: 1024px){
-  width: 960px;
-}
-@media (min-width: 1500px){
-  width: 1436px;
-}
-`
-export const CenterContent = (props: Props) => {
-  const {children, ref} = props
-  return (
-    <div className={center} ref={ref}>
-      {children}
-    </div>
-  )
-}
+export const CenterContent = ({ children }: PropsWithChildren<Props>) => (
+  <div
+    css={css`
+      border: 5px solid black;
+      margin: auto;
+      width: 100%;
+
+      @media (min-width: 480px) {
+        width: 416px;
+      }
+
+      @media (min-width: 768px) {
+        width: 704px;
+      }
+
+      @media (min-width: 1024px) {
+        width: 960px;
+      }
+
+      @media (min-width: 1500px) {
+        width: 1436px;
+      }
+    `}
+  >
+    {children}
+  </div>
+)
